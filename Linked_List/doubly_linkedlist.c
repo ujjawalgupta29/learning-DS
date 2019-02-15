724039cc85015120b3ca973458dc64b22ab1d5e1
Ujjawal Gupta <gupta.ujjawal29@gmail.com>
2019-02-15 16:07:27 +0530

    insertion

--- Linked_List/doubly_linkedlist.c
+++ Linked_List/doubly_linkedlist.c
@@ -38,6 +38,88 @@ struct node * add_at_beg(struct node * start)
     return start;
 };
 
+struct node * add_at_end(struct node * start)
+{
+    struct node *p =start;
+    struct node * temp = (struct node *)malloc(sizeof(struct node));
+    printf("Enter data to be inserted at end:- ");
+    int n;
+    scanf("%d",&n);
+
+
+    while(p->next != NULL)
+    {
+        p = p->next;
+    }
+    temp->info = n;
+    //printf("check1\n");
+    temp->next = NULL;
+    //printf("check2\n");
+    temp->prev = p;
+    //printf("check3\n");
+
+    if(start != NULL)
+        p->next = temp;
+
+    //printf("check4\n");
+    return start;
+};
+
+struct node * add_after(struct node * start)
+{
+    printf("Data of node after which new node to be inserted:- ");
+    int data;
+    scanf("%d",&data);
+    printf("Enter data to be inserted after selected node:- ");
+    int n;
+    scanf("%d",&n);
+
+    struct node *p =start;
+    struct node * temp = (struct node *)malloc(sizeof(struct node));
+
+    while(p->info != data)
+    {
+        p = p->next;
+    }
+    temp->info = n;
+    temp->next = p->next;
+    temp->prev = p;
+
+    p->next->prev = temp;
+    p->next = temp;
+
+    return start;
+};
+
+struct node * add_before(struct node * start)
+{
+    printf("Data of node before which new node to be inserted:- ");
+    int data;
+    scanf("%d",&data);
+    printf("Enter data to be inserted after selected node:- ");
+    int n;
+    scanf("%d",&n);
+
+    struct node *p =start;
+    struct node * temp = (struct node *)malloc(sizeof(struct node));
+    temp->info = n;
+
+    while(p->info != data)
+    {
+        p = p->next;
+    }
+
+    temp->prev = p->prev;
+    temp->next = p;
+
+    p->prev->next = temp;
+    p->prev = temp;
+
+    return start;
+
+};
+
+
 void traverse(struct node *start)
 {
     struct node * p = start;
@@ -67,6 +149,11 @@ int main()
     struct node * start = NULL;
 
     start = add_at_beg(start);
+    start = add_at_end(start);
+    start = add_at_end(start);
+    start = add_after(start);
+    start = add_before(start);
+    start = add_before(start);
     traverse(start);
     counts(start);
 }
