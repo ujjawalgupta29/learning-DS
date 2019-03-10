7f6719c44756fe9f9596998aad595e17f424798e
Ujjawal Gupta <gupta.ujjawal29@gmail.com>
2019-03-10 18:00:26 +0530

    postfix evaluator

--- stacks/expression_to_postfix.cpp
+++ stacks/expression_to_postfix.cpp
@@ -13,6 +13,89 @@ int precedence(char ch)
     return 0;
 }
 
+int apply_operation(int value1,int value2, char ch)
+{
+    switch (ch)
+    {
+        case '-': return value1-value2;
+        case '+': return value1+value2;
+        case '*': return value1*value2;
+        case '/': return value1/value2;
+        case '^': return pow(value1,value2);
+    }
+    return -1;
+}
+
+void postfix(string s)
+{
+
+    stack <char> operations;
+    string post;
+
+
+    for(int i = 0 ;i < s.length() ; i++)
+    {
+        char ch = s[i];
+        //cout<<"ch= "<<ch<<endl;
+        if(ch  == ' ')
+        {
+            //cout<<"space"<<endl;
+            continue;
+        }
+
+        else if(ch == '(')
+            operations.push(s[i]);
+
+        // if character is an operand
+        else if(  (s[i] <= 'z' && s[i] >= 'a') || (s[i] <= 'Z' && s[i] >= 'A')  )
+        {
+            //cout<<"operand"<<endl;
+            post = post + ch;
+        }
+
+        else if(ch == ')')
+        {
+
+            while( !operations.empty() && operations.top() != '(')
+            {
+                char character = operations.top();
+                operations.pop();
+                post = post + character;
+
+            }
+            operations.pop();
+
+        }
+
+        //if we encounter a operator
+        else
+        {
+
+            while(!operations.empty() &&  operations.top() != '(' && precedence(s[i]) <= precedence(operations.top()) )
+            {
+                char character = operations.top();
+                operations.pop();
+                post = post + character;
+
+            }
+
+            operations.push(ch);
+
+        }
+
+    }
+
+    while(!operations.empty())
+    {
+        char character = operations.top();
+        operations.pop();
+        post = post + character;
+    }
+
+    cout<<post<<endl;
+
+}
+
 
 int main()
 {
@@ -20,5 +103,5 @@ int main()
     cout<<"Enter expression to be converted: "<<endl;
     getline(cin,s);
 
-    cout<<postfix(s);
+    postfix(s);
 }
